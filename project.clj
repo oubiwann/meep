b0efdd4871ca3abbf4cf07046146f50a869b3a6f
Duncan McGreggor <oubiwann@gmail.com>
2015-10-18 00:08:04 -0500

    Added initial project files.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject meep "0.1.0-SNAPSHOT"
+  :description "A Clojure Twitter GUI Client"
+  :url "https://github.com/oubiwann/meep"
+  :license {:name "Apache License, Version 2.0"
+            :url "https://opensource.org/licenses/Apache-2.0"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [twitter-api "0.7.8"]
+                 [seesaw "1.4.5"]])
