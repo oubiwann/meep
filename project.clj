9eb5320534aecab586f54a54d08d54bf9cf122c0
Duncan McGreggor <oubiwann@gmail.com>
2015-10-18 20:12:11 -0500

    Added logging and file system util deps.

--- project.clj
+++ project.clj
@@ -4,5 +4,8 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://opensource.org/licenses/Apache-2.0"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [twitter-api "0.7.8"]
-                 [seesaw "1.4.5"]])
+                 [seesaw "1.4.5"]
+                 [ch.qos.logback/logback-classic "1.1.3"]
+                 [me.raynes/fs "1.4.6"]])
